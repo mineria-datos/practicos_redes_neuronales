279b340f9c6a4ab4f70243e512b7128e0b16f8f2
Zacarias F. Ojeda <zojeda@gmail.com>
2020-01-22 15:38:07 -0300

    inicio algoritmo genetico

--- GUIA 3/Genetica.R
+++ GUIA 3/Genetica.R
@@ -31,6 +31,7 @@ bit2int <- function(x) {
 crearPoblacion <- function(cantidadIndividuos, cantidadVariables, limiteInf = NULL,
                             limiteSup = NULL, verbose = TRUE) {
   # Matriz donde almacenar los individuos generados.
+  # cada renglon es una posible solución
   poblacion <- matrix(data = NA, nrow = cantidadIndividuos, ncol = cantidadVariables)
   
   # Bucle para crear cada individuo.
@@ -106,4 +107,92 @@ mutar <- function(x, cantidadBits, p = 0.001, xMin, xMax) {
   if(x>xMax) x <- xMax
   if(x<xMin) x <- xMin
   return(x)
+}
+
+
+algoritmoGenetico <- function(
+  cantidadIndividuos = 100,
+  cantidadVariables = 1,
+  limiteInf, limiteSup,
+  fitnessFn,
+  nPoblacion=100,
+  nGeneraciones=100,
+  generacionesSinCambio=10,
+  toleraciaSinCambio=0.01 # valor minimo de diferencia entre generaciones para considerar cambio en la mejor solucion
+  ) {
+
+  # ALMACENAMIENTO DE RESULTADOS
+  # ============================================================================
+  # Por cada generación se almacena el mejor individuo, su fitness, y la diferencia
+  # absoluta respecto a la última generación.
+  resultados_fitness   <- vector(mode = "list", length = nGeneraciones)
+  resultados_individuo <- vector(mode = "list", length = nGeneraciones)
+  diferencia_abs       <- vector(mode = "list", length = nGeneraciones)
+  
+  
+  # 1. Creamos poblacion inicial aleatoria.
+
+  poblacion <- crearPoblacion(cantidadIndividuos, cantidadVariables, limiteInf = limiteInf,
+                                limiteSup = limiteSup)
+  
+  
+  for(i in 1:nGeneraciones) {
+    
+    # 2. Calcular Aptitud
+    
+    aptitud <- aptitud(poblacion, fitnessFn)
+    
+    # mejor  solución en la iteración actual
+    fitness_mejor_individuo   <- max(aptitud)
+    print(glue::glue("fitness_mejor_individuo: {fitness_mejor_individuo}"))
+    mejor_individuo           <- poblacion[which.max(aptitud), ]
+    resultados_fitness[[i]]   <- fitness_mejor_individuo
+    resultados_individuo[[i]] <- mejor_individuo
+    if (i > 1) {
+      diferencia_abs[[i]] <- abs(resultados_fitness[[i - 1]] - resultados_fitness[[i]])
+    }
+    
+    nuevaPoblacion <- matrix(data = NA, nrow = cantidadIndividuos, ncol = cantidadVariables)
+    
+    for (j in 1:nPoblacion) {
+      # 3. Selección de Individuos
+      
+      individuoA <- seleccion(aptitud, metodo = "ranking")
+      individuoB <- seleccion(aptitud, metodo = "ranking")
+      
+      # 4. Cruza de Individuos
+      
+      individuoAB <- cruzar(poblacion[individuoA], poblacion[individuoB],
+                            cantidadBits, metodo = "mitad")
+      
+      # 5. Mutación de Individuo
+      
+      individuoAB <- mutar(individuoAB, cantidadBits, p = 0.01, 
+                           xMin = xMin, xMax = xMax)
+      
+      nuevaPoblacion[j, ] <- individuoAB
+    }
+    
+    poblacion <- nuevaPoblacion
+    
+    if (i > generacionesSinCambio) {
+      ultimasDiferencias <- tail(unlist(diferencia_abs), n = generacionesSinCambio)
+      if (all(ultimasDiferencias < toleraciaSinCambio)) {
+        print(glue::glue("freno por no haber cambios "))
+        break()
+      }
+    }
+    
+  }
+  
+  # IDENTIFICACIÓN DEL MEJOR INDIVIDUO DE TODO EL PROCESO
+  # ==========================================================================
+  mejor_individuo <- resultados_individuo[[which.max(unlist(resultados_fitness))]]
+  print(glue::glue("mejor individuo: ", mejor_individuo))
+  
+  return( 
+    list(
+      mejor_individuo = mejor_individuo  
+    )
+  )
 }
