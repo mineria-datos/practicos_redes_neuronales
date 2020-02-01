7264708301a9003b8f506a998713ce44b808d95c
Zacarias F. Ojeda <zojeda@gmail.com>
2020-02-01 20:07:46 -0300

    wip: libro con resoluciones

--- GUIA_1/PerceptronMulticapa.R
+++ GUIA_1/PerceptronMulticapa.R
@@ -56,7 +56,7 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
   nroEntradas <- dim(datos)[2] - nroSalidas
   cantidadDatos <- dim(datos)[1]
   nroCapas <- length(arquitectura)
-
+  
   # Inicializando con 1
   #w <- list()
   #w[[1]] <- matrix(rep(1, (nroEntradas+1)*(arquitectura[1])),
@@ -65,18 +65,18 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
   #  w[[j+1]]<- matrix(rep(1, (arquitectura[j]+1)*arquitectura[j+1]),
   #                    ncol = arquitectura[j]+1, nrow = arquitectura[j+1]) %>% as.matrix()
   #}
-
+  
   # Inicializando random. 
   w <- list()
   set.seed(semilla)
   w[[1]] <- matrix(runif((nroEntradas+1)*(arquitectura[1]),-0.5,0.5),
                    ncol = (nroEntradas+1), nrow = arquitectura[1]) %>% as.matrix()
-  for (j in seq(1:(nroCapas-1))) {
+  for (j in seq(1:(length(arquitectura)-1))) {
     w[[j+1]]<- matrix(runif((arquitectura[j]+1)*arquitectura[j+1],-0.5,0.5),
                       ncol = arquitectura[j]+1, nrow = arquitectura[j+1]) %>% as.matrix()
   }
   #print(w)
-
+  
   # Tenemos una matriz W por cada capa definida en la arquitectura.
   # Cada fila de W se define con el numero de neuronas
   # Cada columna de W se definie con el numero de entradas mas uno por el w0
@@ -90,14 +90,13 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
   
   calcularForward <- function(x) {
     y <- list()
-    x <- t(x)
-    x <- rbind(-1,x) %>% as.matrix()
-    y[[1]] <- sigmoidea(w[[1]] %*% x)
+    x <- cbind(-1,x) %>% as.matrix()
+    y[[1]] <- sigmoidea(w[[1]] %*% t(x)) %>% as.matrix()
     
-    for (j in seq(2,nroCapas)) {
-      x <- y[[j-1]]
-      x <- rbind(-1, x)
-      y[[j]] <- sigmoidea(w[[j]] %*% x)
+    for (j in seq(1:(length(arquitectura)-1))) {
+      x <- y[[j]]
+      x <- rbind(-1,x) %>% as.matrix()
+      y[[j+1]] <- sigmoidea(w[[j+1]] %*% x) %>% as.matrix()
     }
     y
   }
@@ -115,21 +114,34 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
       # Backward
       #print("Backward")
       yd <- datos[i, seq(nroEntradas+1,nroSalidas+nroEntradas)]
-      error <- t(yd) - y[[nroCapas]]
+      error <- (yd - y[[nroCapas]]) %>% as.numeric()
       
-
-
-      #En capas intermedias y salida
-      for (k in seq(nroCapas,2)) {
-        if (k==nroCapas) {
-          l_k = error
-        } else {
-          l_k = t(w[[k+1]]) %*% d[[k+1]]
-        }
-        d[[k]]  <- l_k[-1] * sigmoidea_derivada(y[[k]])
-        dw[[k]] <- nu * d[[k]] %*% t(rbind(-1,y[[k-1]]))
+      
+      #En capa de salida
+      d[[nroCapas]]  <- error * sigmoidea_derivada(y[[nroCapas]]) %>% as.matrix()
+      if((dim(d[[nroCapas]])[1] == 1) && (dim(d[[nroCapas]])[2] == 1)){
+        # caso en que dw es un numero porque tenemos una sola salida.
+        dw[[nroCapas]] <- t(nu * as.numeric(d[[nroCapas]]) * (rbind(-1,y[[nroCapas-1]])
+                                                              %>% as.matrix()))
+      } else{
+        dw[[nroCapas]] <- nu * d[[nroCapas]] %*% t(rbind(-1,y[[nroCapas-1]])
+                                                   %>% as.matrix())
       }
-
+      
+      #En capas intermedias
+      if(nroCapas > 2) {
+        #print("test - capas mayor a 2")
+        for (k in seq(nroCapas-1,2)) {
+          d[[k]]  <- (t(w[[k+1]]) %*% d[[k+1]])[-1] * sigmoidea_derivada(y[[k]]) %>% as.matrix()
+          if((dim(d[[k]])[1] == 1) && (dim(d[[k]])[2] == 1)){
+            # caso en que dw es un numero porque tenemos una sola salida.
+            dw[[k]] <- t(nu * as.numeric(d[[k]]) * (rbind(-1,y[[k-1]]) %>% as.matrix()))
+          } else{
+            dw[[k]] <- nu * d[[k]] %*% t(rbind(-1,y[[k-1]]) %>% as.matrix())
+          }
+        }
+      }
+      
       #En la capa de entrada
       d[[1]]  <- (t(w[[2]]) %*% d[[2]])[-1] * sigmoidea_derivada(y[[1]]) %>% as.matrix()
       x <- datos[i, 1:nroEntradas]
@@ -140,7 +152,7 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
       } else {
         dw[[1]] <- nu * d[[1]] %*% x
       }
-    
+      
       # Corrección de pesos
       #print("Correccion de Pesos")
       if (e > 1) {
@@ -154,9 +166,9 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
       }
       dw_aux <- dw # Guardo el delta anterior para el termino de momento
       
-    
+      
     }
-
+    
     # Calculo la salida para todos los datos
     # print("Calculo de salida.")
     salida <- matrix(0, nrow = cantidadDatos, ncol = nroSalidas)
@@ -186,7 +198,7 @@ entrenarPerceptronM <- function(datos, critFinalizacion, maxEpocas, nu=0.05, sem
     if(tasa > critFinalizacion) break
     
   }
-
+  
   vectorError <- v_e2
   resultado <- list("tasa" = tasa, "w" = w, "error" = vectorError, "resultado" = salida, "epocas" = e, "predecir" = function(entrada) {
     y = calcularForward(entrada[,1:nroEntradas])
